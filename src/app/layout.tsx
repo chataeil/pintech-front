b83f4fd18c62fa1ddf9dd4f22f0c564fc831ad96
chataeil <ckxodlf12@naver.com>
2025-02-05 15:14:31 +0900

    add

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import StyledJsxRegistry from "./registry";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <StyledJsxRegistry>{children}</StyledJsxRegistry>
+      </body>
+    </html>
+  );
+}
